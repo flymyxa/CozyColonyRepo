bccd0753798d8907c5b9584ecd5c57740300b5d0
Serhii Komar <ceo@komargames.com>
2025-07-08 16:50:21 -0700

    Remove unused job and input system usings

--- CozyColony/Assets/Scripts/BuildMenuUI.cs
+++ CozyColony/Assets/Scripts/BuildMenuUI.cs
@@ -1,10 +1,9 @@
 ﻿using CozyWorld;
 using System.Collections.Generic;
 using UnityEngine.Events;
-using UnityEngine.InputSystem;
 using UnityEngine;
 using UnityEngine.UI; // for UI Button
-using TMPro;     
+using TMPro;
 
 public class BuildMenuUI : MonoBehaviour
 {
@@ -24,58 +23,3 @@ public class BuildMenuUI : MonoBehaviour
     }
 }
 
-public class PlacementGhost : MonoBehaviour
-{
-    [SerializeField] private Material validMat;
-    [SerializeField] private Material invalidMat;
-
-    private BuildDef current;
-    private GameObject ghostInstance;
-    private bool isPlacing;
-
-    private void OnEnable()
-    {
-        FindFirstObjectByType<BuildMenuUI>().OnBuildSelected.AddListener(StartPlacing);
-    }
-
-    private void Update()
-    {
-        if (!isPlacing) return;
-        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-        if (Physics.Raycast(ray, out RaycastHit hit, 100f))
-        {
-            Vector3 snap = hit.point;
-            Vector2Int cell = GridManager.Instance.WorldToCell(snap);
-            ghostInstance.transform.position = GridManager.Instance.CellToWorld(cell);
-
-            bool canPlace = Validate(cell);
-            SetGhostMat(canPlace ? validMat : invalidMat);
-
-            if (Mouse.current.leftButton.wasReleasedThisFrame && canPlace)
-                ConfirmPlacement(cell);
-        }
-    }
-
-    private void StartPlacing(BuildDef def)
-    {
-        current = def;
-        if (ghostInstance != null) Destroy(ghostInstance);
-        ghostInstance = Instantiate(def.prefab);
-        ghostInstance.transform.localScale = Vector3.one; // ensure scale
-        isPlacing = true;
-    }
-
-    private void ConfirmPlacement(Vector2Int cell)
-    {
-        Instantiate(current.prefab, GridManager.Instance.CellToWorld(cell), Quaternion.identity);
-        GridManager.Instance.SetWalkable(cell, false);
-        Destroy(ghostInstance);
-        isPlacing = false;
-    }
-
-    private bool Validate(Vector2Int cell) => GridManager.Instance.IsWalkable(cell);
-    private void SetGhostMat(Material m)
-    {
-        foreach (var mr in ghostInstance.GetComponentsInChildren<MeshRenderer>()) mr.sharedMaterial = m;
-    }
-}
